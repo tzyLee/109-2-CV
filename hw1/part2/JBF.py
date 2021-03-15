46f8e0516f50da4ea76c61fd8a0d1520c6293444
tzyLee <b06901145@ntu.edu.tw>
2021-03-15 12:26:57 +0800

    Implement JBF

--- hw1/part2/JBF.py
+++ hw1/part2/JBF.py
@@ -6,65 +6,59 @@ class Joint_bilateral_filter(object):
     def __init__(self, sigma_s, sigma_r):
         self.sigma_r = sigma_r
         self.sigma_s = sigma_s
-        self.wndw_size = ws = 6 * sigma_s + 1
-        self.pad_w = r = 3 * sigma_s
+        self.wndw_size = 6 * sigma_s + 1
+        self.pad_w = 3 * sigma_s
 
+    def joint_bilateral_filter(self, img, guidance):
+        r = self.pad_w
+        ws = self.wndw_size
+        sigma_s = self.sigma_s
+        sigma_r = self.sigma_r
+
+        # Image padding
+        BORDER_TYPE = cv2.BORDER_REFLECT
+        I = cv2.copyMakeBorder(img, r, r, r, r, BORDER_TYPE)
+        G = cv2.copyMakeBorder(guidance, r, r, r, r, BORDER_TYPE).astype(np.int32)
+        h, w, ch = img.shape
+
+        output = np.zeros_like(img, dtype=np.float64)
+        den = np.zeros((h, w))
         # Pre-compute range kernel value (Gaussian(-255^2, sigma_r) ~ Gaussian(255^2, sigma_r))
         cache = (np.arange(255 * 2 + 1) - 255) / 255
         cache **= 2
         cache /= -2 * sigma_r * sigma_r
         np.exp(cache, out=cache)
-        self.cache = cache
 
-        # Pre-compute spatial kernel
-        ind = (np.arange(ws) - r) ** 2
-        kernel_s = -(ind + ind[:, np.newaxis]) / (2 * sigma_s * sigma_s)
-        np.exp(kernel_s, out=kernel_s)
-        self.kernel_s = kernel_s[..., np.newaxis, np.newaxis]
-
-    def joint_bilateral_filter(self, img, guidance):
-        # Add padding
-        BORDER_TYPE = cv2.BORDER_REFLECT
-        padded_img = cv2.copyMakeBorder(
-            img, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE
-        )
-        padded_guidance = cv2.copyMakeBorder(
-            guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE
-        )
-
-        # Force the shape to be (H, W, ch)
-        if padded_guidance.ndim == 2:
-            padded_guidance = padded_guidance[..., np.newaxis]
-
-        ws = self.wndw_size
-        r = self.pad_w
-        # Extract the view of shape (ws, ws, H, W, ch)
-        h, w, ch = padded_img.shape
-        img_blocks = np.lib.stride_tricks.as_strided(
-            padded_img,
-            shape=(ws, ws, *img.shape),
-            strides=(w * ch, ch, w * ch, ch, 1),
-            writeable=False,
-        )
-
-        ch = padded_guidance.shape[2]
-        guidance_blocks = np.lib.stride_tricks.as_strided(
-            padded_guidance,
-            shape=(ws, ws, *img.shape[:2], ch),
-            strides=(w * ch, ch, w * ch, ch, 1),
-            writeable=False,
-        )
-
-        # range part, multiply over all channels
-        # shape = (ws, ws, H, W)
-        diff = guidance_blocks.astype(np.int16)
-        diff -= guidance_blocks[r, r, ...]
-        kernel = (self.cache[diff + 255].prod(axis=-1)) * self.kernel_s
-
-        # shape = (H, W, ch)
-        output = (kernel[..., np.newaxis] * img_blocks).sum(axis=(0, 1)) / (
-            kernel.sum(axis=(0, 1))[..., np.newaxis]
-        )
+        # Main body
+        if I.ndim == 3 and G.ndim == 2:
+            center = G[r : r + h, r : r + w]
+            for offset in range(ws ** 2):
+                y, x = divmod(offset, ws)
+                # scalar
+                kernel_s = np.exp(
+                    -((y - r) ** 2 + (x - r) ** 2) / (2 * sigma_s * sigma_s)
+                )
+                # (h, w)
+                kernel = cache[G[y : y + h, x : x + w] - center + 255] * kernel_s
+                output += kernel[..., np.newaxis] * I[y : y + h, x : x + w, :]
+                den += kernel
+            output /= den[..., np.newaxis]
+        elif I.ndim == 3 and G.ndim == 3:
+            center = G[r : r + h, r : r + w, :]
+            for offset in range(ws ** 2):
+                y, x = divmod(offset, ws)
+                # scalar
+                kernel_s = np.exp(
+                    -((y - r) ** 2 + (x - r) ** 2) / (2 * sigma_s * sigma_s)
+                )
+                # (h, w)
+                kernel = (
+                    cache[G[y : y + h, x : x + w, :] - center + 255].prod(axis=-1)
+                    * kernel_s
+                )
+                output += kernel[..., np.newaxis] * I[y : y + h, x : x + w, :]
+                den += kernel
+            output /= den[..., np.newaxis]
 
         np.clip(output, 0, 255, out=output)
         return output.astype(np.uint8)
