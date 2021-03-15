28a24d54a9407c428fec7d1c7a5ff9a1b71100b0
tzyLee <b06901145@ntu.edu.tw>
2021-03-15 15:45:13 +0800

    Implement main.py

--- hw1/part1/main.py
+++ hw1/part1/main.py
@@ -5,17 +5,36 @@ from HCD import Harris_corner_detector
 
 
 def main():
-    parser = argparse.ArgumentParser(description='main function of Harris corner detector')
-    parser.add_argument('--threshold', default=100., type=float, help='threshold value to determine corner')
-    parser.add_argument('--image_path', default='./testdata/1.png', help='path to input image')
+    parser = argparse.ArgumentParser(
+        description="main function of Harris corner detector"
+    )
+    parser.add_argument(
+        "--threshold",
+        default=100.0,
+        type=float,
+        help="threshold value to determine corner",
+    )
+    parser.add_argument(
+        "--image_path", default="./testdata/1.png", help="path to input image"
+    )
     args = parser.parse_args()
 
-    print('Processing %s ...'%args.image_path)
+    print("Processing %s ..." % args.image_path)
     img = cv2.imread(args.image_path)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.float64)
 
-    ### TODO ###
+    hcd = Harris_corner_detector(args.threshold)
+    response = hcd.detect_harris_corners(img_gray)
+    result = hcd.post_processing(response)
+
+    for y, x in result:
+        cv2.circle(img, (x, y), 1, (0, 0, 255), -1)
+    cv2.imshow("result", img)
+
+    key = cv2.waitKey()
+    cv2.destroyAllWindows()
+    cv2.imwrite(f"{args.image_path}_{args.threshold}.png", img)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- hw1/part2/main.py
+++ hw1/part2/main.py
@@ -6,17 +6,74 @@ from JBF import Joint_bilateral_filter
 
 
 def main():
-    parser = argparse.ArgumentParser(description='main function of joint bilateral filter')
-    parser.add_argument('--image_path', default='./testdata/1.png', help='path to input image')
-    parser.add_argument('--setting_path', default='./testdata/1_setting.txt', help='path to setting file')
+    parser = argparse.ArgumentParser(
+        description="main function of joint bilateral filter"
+    )
+    parser.add_argument(
+        "--image_path", default="./testdata/1.png", help="path to input image"
+    )
+    parser.add_argument(
+        "--setting_path",
+        default="./testdata/1_setting.txt",
+        help="path to setting file",
+    )
     args = parser.parse_args()
 
     img = cv2.imread(args.image_path)
-    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ### TODO ###
+    with open(args.setting_path, "r") as f:
+        points = []
+        sigma_r = sigma_s = 0
+
+        lines = iter(f)
+        next(lines)
+        for line in f:
+            vals = line.strip().split(",")
+            if vals[0].startswith("sigma"):
+                sigma_s = int(vals[1])
+                sigma_r = float(vals[3])
+            else:
+                points.append([float(num) for num in vals])
+
+    jbf = Joint_bilateral_filter(sigma_s, sigma_r)
+    bf = jbf.joint_bilateral_filter(img, img)
+    cv2.imshow("bf", bf)
+
+    grays = []
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grays.append(gray)
+    cv2.imshow("cv2 gray", gray)
+    cv2.imwrite(f"{args.image_path}_cv2_gray.png", gray)
+
+    for r, g, b in points:
+        weights = np.array([b, g, r])
+        gray = (img * weights).sum(axis=-1).astype(np.uint8)
+        grays.append(gray)
+        cv2.imshow(f"r={r} g={g} b={b}", gray)
+        cv2.imwrite(f"{args.image_path}_{r}_{g}_{b}.png", gray)
+
+    minInd = 0
+    minDiff = float("inf")
+    for ind, gray in enumerate(grays):
+        out = jbf.joint_bilateral_filter(img, gray)
+        l1_norm = out.astype(np.int32) - bf
+        np.abs(l1_norm, out=l1_norm)
+
+        diff = l1_norm.sum()
+        if diff < minDiff:
+            minDiff = diff
+            minInd = ind
+
+    if minInd == 0:
+        print("best weights are default weights")
+    else:
+        print("best weights are", points[minInd - 1])
+    key = cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
