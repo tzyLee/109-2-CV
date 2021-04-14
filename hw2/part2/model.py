4664b3f9fb003b7129cf5b435103b64139d67524
tzyLee <b06901145@ntu.edu.tw>
2021-04-14 11:17:08 +0800

    Add hw2

---
+++ hw2/part2/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        # TODO
+
+    def forward(self, x):
+        # TODO
+        return out
+
+    def name(self):
+        return "ConvNet"
+
+class MyNet(nn.Module):
+    def __init__(self):
+        super(MyNet, self).__init__()
+        # TODO
+
+    def forward(self, x):
+        # TODO
+        return out
+
+    def name(self):
+        return "MyNet"
+
