4b44860e312fce1e48334a276a2e0442c4a1af60
tzyLee <b06901145@ntu.edu.tw>
2021-04-17 21:31:05 +0800

    Use 24x24 tiny image feature

--- hw2/part1/get_tiny_images.py
+++ hw2/part1/get_tiny_images.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def get_tiny_images(image_paths):
     #############################################################################
     # To build a tiny image feature, simply resize the original image to a very #
@@ -9,19 +10,19 @@ def get_tiny_images(image_paths):
     # square portion out of each image. Making the tiny images zero mean and    #
     # unit length (normalizing them) will increase performance modestly.        #
     #############################################################################
-    '''
+    """
     Input :
         image_paths: a list(N) of string where each string is an image
         path on the filesystem.
     Output :
         tiny image features : (N, d) matrix of resized and then vectorized tiny
         images. E.g. if the images are resized to 16x16, d would equal 256.
-    '''
+    """
     n = len(image_paths)
-    tiny_images = np.zeros((n, 16*16))
+    tiny_images = np.zeros((n, 24 * 24))
     for i, path in enumerate(image_paths):
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-        resized = cv2.resize(img, (16, 16))
+        resized = cv2.resize(img, (24, 24))
         tiny_images[i, :] = resized.ravel()
 
     # zero mean
