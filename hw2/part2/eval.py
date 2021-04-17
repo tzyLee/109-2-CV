6c42386927892315992234d645ca775ac58c83ff
tzyLee <b06901145@ntu.edu.tw>
2021-04-17 23:43:16 +0800

    Use abspath and mkdir

--- hw2/part2/eval.py
+++ hw2/part2/eval.py
@@ -13,16 +13,19 @@ from data import TestDataset
 if __name__ == "__main__":
     data_path, model_type, output = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    if model_type == 'conv':
+    if model_type == "conv":
         model = ConvNet()
-    elif model_type == 'mynet':
+    elif model_type == "mynet":
         model = MyNet()
 
     #######################################################################
     # Modifiy this part to load your trained model
-    # TODO
     #######################################################################
-
+    checkpoint_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "{}.pth".format(model.name())
+    )
+    print("Loading checkpoint from:", checkpoint_path)
+    model.load_state_dict(torch.load(checkpoint_path))
 
     use_cuda = torch.cuda.is_available()
     if use_cuda:
@@ -30,24 +33,30 @@ if __name__ == "__main__":
     model.eval()
 
     # Load data
-    trans = transforms.Compose([transforms.Grayscale(),transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+    trans = transforms.Compose(
+        [
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (1.0,)),
+        ]
+    )
     test_set = TestDataset(data_path, transform=trans)
-    print('Length of Testing Set:', len(test_set))
+    print("Length of Testing Set:", len(test_set))
     test_loader = DataLoader(dataset=test_set, batch_size=1, shuffle=False)
 
     # testing
     prediction = []
     with torch.no_grad():
-        for batch_idx, (x,name) in enumerate(test_loader):
+        for batch_idx, (x, name) in enumerate(test_loader):
             if use_cuda:
                 x = x.cuda()
             out = model(x)
             _, pred_label = torch.max(out, 1)
             prediction.append((name[0][:-4], pred_label.item()))
 
-    with open(output, 'w', newline='') as csvfile:
+    os.makedirs(os.path.dirname(output), exist_ok=True)
+    with open(output, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['id','label'])
+        writer.writerow(["id", "label"])
         for i in prediction:
             writer.writerow([i[0], i[1]])
-
