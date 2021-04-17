d73b1c3aadadef6623c91735f206d299db3378c5
tzyLee <b06901145@ntu.edu.tw>
2021-04-17 21:30:25 +0800

    Add hw2 part2

--- hw2/part2/model.py
+++ hw2/part2/model.py
@@ -2,27 +2,63 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class ConvNet(nn.Module):
     def __init__(self):
-        super(ConvNet, self).__init__()
-        # TODO
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 6, 5),  # (N, 1, 28, 28) -> (N, 6, 24, 24)
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),  # (N, 6, 12, 12)
+            nn.Conv2d(6, 16, 5),  # (N, 6, 12, 12) -> (N, 16, 8, 8)
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),  # (N, 16, 4, 4)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16 * 4 * 4, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120, 84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84, 10),
+        )
 
     def forward(self, x):
-        # TODO
-        return out
+        out = self.flatten(self.cnn(x))
+        return self.fc(out)
 
     def name(self):
         return "ConvNet"
 
+
 class MyNet(nn.Module):
     def __init__(self):
-        super(MyNet, self).__init__()
-        # TODO
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 64, 5),  # (N, 1, 28, 28) -> (N, 64, 24, 24)
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),  # (N, 64, 12, 12)
+            nn.Conv2d(64, 32, 5),  # (N, 64, 12, 12) -> (N, 32, 8, 8)
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),  # (N, 32, 4, 4)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(32 * 4 * 4, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(128, 64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(64, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 10),
+        )
 
     def forward(self, x):
-        # TODO
-        return out
+        return self.fc(self.flatten(self.cnn(x)))
 
     def name(self):
         return "MyNet"
-
--- hw2/part2/train.py
+++ hw2/part2/train.py
@@ -11,17 +11,17 @@ from data import get_dataloader
 if __name__ == "__main__":
     # Specifiy data folder path and model type(fully/conv)
     folder, model_type = sys.argv[1], sys.argv[2]
-    
+
     # Get data loaders of training set and validation set
     train_loader, val_loader = get_dataloader(folder, batch_size=32)
 
     # Specify the type of model
-    if model_type == 'conv':
+    if model_type == "conv":
         model = ConvNet()
-    elif model_type == 'mynet':
+    elif model_type == "mynet":
         model = MyNet()
 
-    # Set the type of gradient optimizer and the model it update 
+    # Set the type of gradient optimizer and the model it update
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
     # Choose loss function
@@ -33,18 +33,23 @@ if __name__ == "__main__":
         model.cuda()
 
     # Run any number of epochs you want
-    ep = 10
+    train_acc = []
+    train_loss = []
+    val_acc = []
+    val_loss = []
+    ep = 20
+    scheduler = optim.lr_scheduler.StepLR(optimizer, 5)
     for epoch in range(ep):
-        print('Epoch:', epoch)
+        print("Epoch:", epoch)
         ##############
         ## Training ##
         ##############
-        
         # Record the information of correct prediction and loss
         correct_cnt, total_loss, total_cnt = 0, 0, 0
-        
+
+        model.train()
         # Load batch data from dataloader
-        for batch, (x, label) in enumerate(train_loader,1):
+        for batch, (x, label) in enumerate(train_loader, 1):
             # Set the gradients to zero (left by previous iteration)
             optimizer.zero_grad()
             # Put input tensor to GPU if it's available
@@ -68,22 +73,99 @@ if __name__ == "__main__":
             # Show the training information
             if batch % 500 == 0 or batch == len(train_loader):
                 acc = correct_cnt / total_cnt
-                ave_loss = total_loss / batch           
-                print ('Training batch index: {}, train loss: {:.6f}, acc: {:.3f}'.format(
-                    batch, ave_loss, acc))
+                ave_loss = total_loss / batch
+                print(
+                    "Training batch index: {}, train loss: {:.6f}, acc: {:.3f}".format(
+                        batch, ave_loss, acc
+                    )
+                )
+        train_acc.append(correct_cnt / total_cnt)
+        train_loss.append(total_loss / total_cnt)
 
+        scheduler.step()
         ################
         ## Validation ##
         ################
-        model.eval()
-        # TODO
-        model.train()
+        # Record the information of correct prediction and loss
+        correct_cnt, total_loss, total_cnt = 0, 0, 0
 
+        model.eval()
+        with torch.no_grad():
+            # Load batch data from dataloader
+            for batch, (x, label) in enumerate(val_loader, 1):
+                # Put input tensor to GPU if it's available
+                if use_cuda:
+                    x, label = x.cuda(), label.cuda()
+                # Forward input tensor through your model
+                out = model(x)
+                # Calculate loss
+                loss = criterion(out, label)
+
+                # Calculate the training loss and accuracy of each iteration
+                total_loss += loss.item()
+                _, pred_label = torch.max(out, 1)
+                total_cnt += x.size(0)
+                correct_cnt += (pred_label == label).sum().item()
+
+                # Show the training information
+                if batch % 100 == 0 or batch == len(val_loader):
+                    acc = correct_cnt / total_cnt
+                    ave_loss = total_loss / batch
+                    print(
+                        "Validation batch index: {}, valid loss: {:.6f}, acc: {:.3f}".format(
+                            batch, ave_loss, acc
+                        )
+                    )
+        val_acc.append(correct_cnt / total_cnt)
+        val_loss.append(total_loss / total_cnt)
     # Save trained model
-    torch.save(model.state_dict(), './checkpoint/%s.pth' % model.name())
+    torch.save(model.state_dict(), "./checkpoint/{}.pth".format(model.name()))
 
     # Plot Learning Curve
-    # TODO
-    
+    x = list(range(1, ep + 1))
+    x_tick = x[::2]
+    plt.figure()
+    plt.grid(True, linestyle="-.")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.plot(x, train_loss, label="Training Loss")
+    plt.legend()
+    plt.title(model_type)
+    plt.tight_layout()
+    plt.xticks(x_tick)
+    plt.savefig("./checkpoint/{}_train_loss.png".format(model_type))
 
-    
+    plt.figure()
+    plt.grid(True, linestyle="-.")
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.plot(x, train_acc, label="Training Accuracy")
+    plt.legend()
+    plt.title(model_type)
+    plt.tight_layout()
+    plt.xticks(x_tick)
+    plt.ylim(top=1)
+    plt.savefig("./checkpoint/{}_train_acc.png".format(model_type))
+
+    plt.figure()
+    plt.grid(True, linestyle="-.")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.plot(x, val_loss, label="Validation Loss")
+    plt.legend()
+    plt.title(model_type)
+    plt.tight_layout()
+    plt.xticks(x_tick)
+    plt.savefig("./checkpoint/{}_val_loss.png".format(model_type))
+
+    plt.figure()
+    plt.grid(True, linestyle="-.")
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.plot(x, val_acc, label="Validation Accuracy")
+    plt.legend()
+    plt.title(model_type)
+    plt.tight_layout()
+    plt.xticks(x_tick)
+    plt.ylim(top=1)
+    plt.savefig("./checkpoint/{}_val_acc.png".format(model_type))
