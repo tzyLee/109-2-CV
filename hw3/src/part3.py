d1e2a785700ea928f75938f59f40176b672db73a
tzyLee <b06901145@ntu.edu.tw>
2021-05-21 18:53:32 +0800

    Finish part3 and part4

--- hw3/src/part3.py
+++ hw3/src/part3.py
@@ -3,19 +3,23 @@ import cv2
 from utils import solve_homography, warping
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # ================== Part 3 ========================
-    secret1 = cv2.imread('../resource/BL_secret1.png')
-    secret2 = cv2.imread('../resource/BL_secret2.png')
+    secret1 = cv2.imread("../resource/BL_secret1.png")
+    secret2 = cv2.imread("../resource/BL_secret2.png")
     corners1 = np.array([[429, 337], [517, 314], [570, 361], [488, 380]])
     corners2 = np.array([[346, 196], [437, 161], [483, 198], [397, 229]])
     h, w, c = (500, 500, 3)
     dst = np.zeros((h, w, c))
 
-    # TODO: call solve_homography() & warping
-    output3_1 = None
-    output3_2 = None
+    dst_corners = np.array([[0, 0], [w, 0], [w, h], [0, h]])
+    # call solve_homography() & warping
+    H = solve_homography(corners1, dst_corners)
+    output3_1 = warping(secret1, dst, H, 0, h, 0, w, direction="b")
+    cv2.imwrite("output3_1.png", output3_1)
 
-    cv2.imwrite('output3_1.png', output3_1)
-    cv2.imwrite('output3_2.png', output3_2)
+    dst[...] = 0
+    H = solve_homography(corners2, dst_corners)
+    output3_2 = warping(secret2, dst, H, 0, h, 0, w, direction="b")
+    cv2.imwrite("output3_2.png", output3_2)
--- hw3/src/part4.py
+++ hw3/src/part4.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
 import random
+from itertools import chain
 from tqdm import tqdm
 from utils import solve_homography, warping
 
 random.seed(999)
+
 
 def panorama(imgs):
     """
@@ -12,28 +14,89 @@ def panorama(imgs):
     :param imgs: list of images to be stitched
     :return: stitched panorama
     """
-    h_max = max([x.shape[0] for x in imgs])
-    w_max = sum([x.shape[1] for x in imgs])
+    h_max = max(x.shape[0] for x in imgs)
+    w_max = sum(x.shape[1] for x in imgs)
 
     # create the final stitched canvas
     dst = np.zeros((h_max, w_max, imgs[0].shape[2]), dtype=np.uint8)
-    dst[:imgs[0].shape[0], :imgs[0].shape[1]] = imgs[0]
-    last_best_H = np.eye(3)
+    dst[: imgs[0].shape[0], : imgs[0].shape[1]] = imgs[0]
+    accumulatedH = np.eye(3)
     out = None
+    orb = cv2.ORB_create()
+    bf = cv2.BFMatcher_create(cv2.NORM_HAMMING, crossCheck=True)
+    rng = np.random.default_rng(seed=42)
 
     # for all images to be stitched:
     for idx in range(len(imgs) - 1):
         im1 = imgs[idx]
         im2 = imgs[idx + 1]
 
-        # TODO: 1.feature detection & matching
+        # 1.feature detection & matching
+        kp1, des1 = orb.detectAndCompute(im1, None)
+        kp2, des2 = orb.detectAndCompute(im2, None)
+        matches = bf.match(des1, des2)
+        nMatch = len(matches)
 
-        # TODO: 2. apply RANSAC to choose best H
+        # Transform List[cv2.KeyPoint] into np.array of [[x, y]]
+        kp1 = np.fromiter(
+            chain.from_iterable(kp1[match.queryIdx].pt for match in matches),
+            dtype=np.float64,
+            count=nMatch * 2,
+        ).reshape((-1, 2))
+        kp2 = np.fromiter(
+            chain.from_iterable(kp2[match.trainIdx].pt for match in matches),
+            dtype=np.float64,
+            count=nMatch * 2,
+        ).reshape((-1, 2))
+        # kp1 padded with ones
+        kp2Full = np.hstack((kp2, np.ones((nMatch, 1), dtype=np.float64)))
 
-        # TODO: 3. chain the homographies
+        # 2. apply RANSAC to choose best H
+        it = 0
+        nIter = 100000
+        maxNInlier = 0
+        maxInlierMask = None
+        nInlierThreshold = nMatch * 0.9
+        distThreshold = 12
+        p = 0.99  # Probability of at least one sample is free of outliers
 
-        # TODO: 4. apply warping
+        # Repeat for N iterations, select the largest inlier set
+        while it < nIter:
+            # Sample s data points to instantiate model
+            sampleIndices = rng.choice(nMatch, 4, replace=False)
+            sample1 = kp1[sampleIndices]
+            sample2 = kp2[sampleIndices]
+            H = solve_homography(sample2, sample1)
 
+            # Find the inliers (within distance threshold t)
+            x2 = kp2Full @ H.T
+            x2[:, :2] /= x2[:, 2, np.newaxis]
+            dist = np.linalg.norm(x2[:, :2] - kp1, axis=1)
+
+            inlierMask = dist < distThreshold
+            nInlier = np.count_nonzero(inlierMask)
+
+            if nInlier > maxNInlier:
+                maxNInlier = nInlier
+                maxInlierMask = inlierMask
+
+            # If size of inliers >= T, re-estimate the model and terminate
+            if maxNInlier >= nInlierThreshold:
+                break
+
+            # Adaptively determine the number of sample
+            outlierRatio = 1 - nInlier / nMatch
+            nIter = np.log(1 - p) / np.log(1 - (1 - outlierRatio) ** 4 + 1e-14)
+            it += 1
+
+        # The homography is re-estimated using all points in the maximum inlier set
+        H = solve_homography(kp2[maxInlierMask], kp1[maxInlierMask])
+
+        # 3. chain the homographies
+        accumulatedH = accumulatedH @ H
+
+        # 4. apply warping
+        out = warping(im2, dst, accumulatedH, 0, h_max, 0, w_max, direction="b")
     return out
 
 
@@ -42,6 +105,9 @@ if __name__ == "__main__":
     # ================== Part 4: Panorama ========================
     # TODO: change the number of frames to be stitched
     FRAME_NUM = 3
-    imgs = [cv2.imread('../resource/frame{:d}.jpg'.format(x)) for x in range(1, FRAME_NUM + 1)]
+    imgs = [
+        cv2.imread("../resource/frame{:d}.jpg".format(x))
+        for x in range(1, FRAME_NUM + 1)
+    ]
     output4 = panorama(imgs)
-    cv2.imwrite('output4.png', output4)
+    cv2.imwrite("output4.png", output4)
