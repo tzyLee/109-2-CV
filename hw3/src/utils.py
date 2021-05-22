49c98960f47aef8bab48bed6b36903e98d76b77b
tzyLee <b06901145@ntu.edu.tw>
2021-05-22 23:22:32 +0800

    Add normalization and interpolation

--- hw3/src/utils.py
+++ hw3/src/utils.py
@@ -19,6 +19,30 @@ def solve_homography(u, v):
     if N < 4:
         print("At least 4 points should be given")
 
+    UMean = np.mean(u, axis=0)
+    UMaxStd = np.std(u, axis=0).max() + 1e-14
+    u = u - UMean
+    u /= UMaxStd
+    S1 = np.array(
+        [
+            [1 / UMaxStd, 0, -UMean[0] / UMaxStd],
+            [0, 1 / UMaxStd, -UMean[1] / UMaxStd],
+            [0, 0, 1],
+        ]
+    )
+
+    VMean = np.mean(v, axis=0)
+    VMaxStd = np.std(v, axis=0).max() + 1e-14
+    v = v - VMean
+    v /= VMaxStd
+    S2 = np.array(
+        [
+            [1 / VMaxStd, 0, -VMean[0] / VMaxStd],
+            [0, 1 / VMaxStd, -VMean[1] / VMaxStd],
+            [0, 0, 1],
+        ]
+    )
+
     # 1.forming A
     A = np.zeros((2 * N, 9))
     A[::2, :2] = u
@@ -34,6 +58,8 @@ def solve_homography(u, v):
     # 2.solve H with A
     _, _, vh = np.linalg.svd(A)
     H = vh[-1, :].reshape((3, 3))
+
+    H = np.linalg.inv(S2) @ H @ S1
     return H
 
 
@@ -88,16 +114,48 @@ def warping(src, dst, H, ymin, ymax, xmin, xmax, direction="b"):
         v = u @ H_inv_T
         v[:, :2] /= v[:, 2, np.newaxis]
         v = v[:, :2]
-        np.around(v, out=v)
+
+        v0 = np.floor(v).astype(np.int32)
+        v1 = v0 + 1
         # 4.calculate the mask of the transformed coordinate (should not exceed the boundaries of source image)
         xy_min = np.array([0, 0])
         xy_max = np.array([w_src, h_src])
         mask = (xy_min <= v).all(axis=1) & (v < xy_max).all(axis=1)
         # 5.sample the source image with the masked and reshaped transformed coordinates
         u = u[mask, :2]
-        v = v[mask, :].astype(np.int32)
+        v0 = v0[mask, :]
+        v1 = v1[mask, :]
+        v = v[mask, :]
+        x = v[:, 0]
+        y = v[:, 1]
+        x0 = v0[:, 0]
+        y0 = v0[:, 1]
+        x1 = v1[:, 0]
+        y1 = v1[:, 1]
+        np.clip(x0, 0, w_src - 1, out=x0)
+        np.clip(y0, 0, h_src - 1, out=y0)
+        np.clip(x1, 0, w_src - 1, out=x1)
+        np.clip(y1, 0, h_src - 1, out=y1)
+
+        dx0 = x - x0
+        dy0 = y - y0
+        dx1 = x1 - x
+        dy1 = y1 - y
+
+        wbr = dx0 * dy0
+        wtr = dx0 * dy1
+        wtl = dx1 * dy1
+        wbl = dx1 * dy0
+
+        interpolated = (
+            wtl[..., None] * src[y0, x0, :]
+            + wbl[..., None] * src[y1, x0, :]
+            + wtr[..., None] * src[y0, x1, :]
+            + wbr[..., None] * src[y1, x1, :]
+        )
+        np.clip(interpolated, 0, 255, out=interpolated)
         # 6. assign to destination image with proper masking
-        dst[u[:, 1], u[:, 0], :] = src[v[:, 1], v[:, 0], :]
+        dst[u[:, 1], u[:, 0], :] = interpolated.astype(dst.dtype)
     elif direction == "f":
         H_T = np.transpose(H)
         # 3.apply H to the source pixels and retrieve (u,v) pixels, then reshape to (ymax-ymin),(xmax-xmin)
