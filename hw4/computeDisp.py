12b8a721122a7f20a19884ccaba2583d5fb20148
tzyLee <b06901145@ntu.edu.tw>
2021-06-03 23:28:49 +0800

    Implement 4 direction cost aggregation

--- hw4/computeDisp.py
+++ hw4/computeDisp.py
@@ -6,6 +6,8 @@ d = 12
 sigmaSpace = 15
 sigmaColor = 20
 lambdaCost = 400
+penalty1 = 0.005
+penalty2 = 0.07
 
 
 def computeLocalBinaryPattern(Img, windowSize=7):
@@ -33,6 +35,73 @@ def popcount64(mat):
     mat[...] = (mat & 0x0000FFFF0000FFFF) + ((mat & 0xFFFF0000FFFF0000) >> 16)
     mat[...] = (mat & 0x00000000FFFFFFFF) + (mat >> 32)
     return mat
+
+
+def costAggregate4(cost):
+    nDisp, h, w = cost.shape
+
+    # Perform 4-direction DP-based cost aggregation
+    # Left to right
+    aggCostLR = np.zeros_like(cost, dtype=np.float32)
+    minCostLR = np.full((nDisp + 2, h), np.inf, dtype=np.float32)
+
+    minCostLR[1:-1, :] = cost[:, :, 0]
+    aggCostLR[:, :, 0] = cost[:, :, 0]
+    minAggCostLR = minCostLR.min(axis=0)
+    for i in range(1, w):
+        minCostLR[1:-1, :] = cost[:, :, i] + np.minimum(
+            np.minimum(minCostLR[:-2, :], minCostLR[2:, :]) + penalty1,
+            np.minimum(minCostLR[1:-1, :], minAggCostLR + penalty2),
+        )
+        aggCostLR[:, :, i] = minCostLR[1:-1, :]
+        minCostLR.min(axis=0, out=minAggCostLR)
+
+    # Right to left
+    aggCostRL = np.zeros_like(cost, dtype=np.float32)
+    minCostRL = np.full((nDisp + 2, h), np.inf, dtype=np.float32)
+
+    minCostRL[1:-1, :] = cost[:, :, -1]
+    aggCostRL[:, :, -1] = cost[:, :, -1]
+    minAggCostRL = minCostRL.min(axis=0)
+    for i in reversed(range(w - 1)):
+        minCostRL[1:-1, :] = cost[:, :, i] + np.minimum(
+            np.minimum(minCostRL[:-2, :], minCostRL[2:, :]) + penalty1,
+            np.minimum(minCostRL[1:-1, :], minAggCostRL + penalty2),
+        )
+        aggCostRL[:, :, i] = minCostRL[1:-1, :]
+        minCostRL.min(axis=0, out=minAggCostRL)
+
+    # Top to bottom
+    aggCostTB = np.zeros_like(cost, dtype=np.float32)
+    minCostTB = np.full((nDisp + 2, w), np.inf, dtype=np.float32)
+
+    minCostTB[1:-1, :] = cost[:, 0, :]
+    aggCostTB[:, 0, :] = cost[:, 0, :]
+    minAggCostTB = minCostTB.min(axis=0)
+    for i in range(1, h):
+        minCostTB[1:-1, :] = cost[:, i, :] + np.minimum(
+            np.minimum(minCostTB[:-2, :], minCostTB[2:, :]) + penalty1,
+            np.minimum(minCostTB[1:-1, :], minAggCostTB + penalty2),
+        )
+        aggCostTB[:, i, :] = minCostTB[1:-1, :]
+        minCostTB.min(axis=0, out=minAggCostTB)
+
+    # Bottom to top
+    aggCostBT = np.zeros_like(cost, dtype=np.float32)
+    minCostBT = np.full((nDisp + 2, w), np.inf, dtype=np.float32)
+
+    minCostBT[1:-1, :] = cost[:, -1, :]
+    aggCostBT[:, -1, :] = cost[:, -1, :]
+    minAggCostBT = minCostBT.min(axis=0)
+    for i in reversed(range(h - 1)):
+        minCostBT[1:-1, :] = cost[:, i, :] + np.minimum(
+            np.minimum(minCostBT[:-2, :], minCostBT[2:, :]) + penalty1,
+            np.minimum(minCostBT[1:-1, :], minAggCostBT + penalty2),
+        )
+        aggCostBT[:, i, :] = minCostBT[1:-1, :]
+        minCostBT.min(axis=0, out=minAggCostBT)
+
+    return aggCostLR + aggCostRL + aggCostTB + aggCostBT
 
 
 def computeDisp(Il, Ir, max_disp):
@@ -66,21 +135,25 @@ def computeDisp(Il, Ir, max_disp):
         costR[disp, :, : w - disp] = cost
         costR[disp, :, w - disp :] = cost[:, np.newaxis, -1]
 
-        # >>> Cost Aggregation
-        # Refine the cost according to nearby costs
-        # [Tips] Joint bilateral filter (for the cost of each disparty)
+    # >>> Cost Aggregation
+    # Refine the cost according to nearby costs
+    # [Tips] Joint bilateral filter (for the cost of each disparty)
+    aggCostL = costAggregate4(costL)
+    aggCostR = costAggregate4(costR)
+
+    for disp in range(0, max_disp + 1):
         xip.jointBilateralFilter(
             Ilf,
-            costL[disp, ...],
-            dst=costL[disp, ...],
+            aggCostL[disp, ...],
+            dst=aggCostL[disp, ...],
             d=d,
             sigmaColor=sigmaColor,
             sigmaSpace=sigmaSpace,
         )
         xip.jointBilateralFilter(
             Irf,
-            costR[disp, ...],
-            dst=costR[disp, ...],
+            aggCostR[disp, ...],
+            dst=aggCostR[disp, ...],
             d=d,
             sigmaColor=sigmaColor,
             sigmaSpace=sigmaSpace,
@@ -89,8 +162,8 @@ def computeDisp(Il, Ir, max_disp):
     # >>> Disparity Optimization
     # Determine disparity based on estimated cost.
     # [Tips] Winner-take-all
-    dispL = np.argmin(costL, axis=0)
-    dispR = np.argmin(costR, axis=0)
+    dispL = np.argmin(aggCostL, axis=0)
+    dispR = np.argmin(aggCostR, axis=0)
 
     # >>> Disparity Refinement
     # Do whatever to enhance the disparity map
